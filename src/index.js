052a05793ffc860cf660b5d3e7bd6dd8b7876360
mariusz17 <qbajt.mariusz.kubat@gmail.com>
2022-04-03 22:50:57 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,179 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const Square1 = ({
+//   value,
+// 	onClick
+// }) => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 	useEffect(() => {
+// 		console.log('constructor');
+// 	}, []);
+
+// 	return (
+// 		<button className="square" onClick={() => onClick()}>
+// 			{value}
+// 		</button>
+// 	)
+// }
+
+function Square(props) {
+	return (
+		<button className="square" onClick={props.onClick}>
+			{props.value}
+		</button>
+	);
+}
+
+// class Square extends React.Component {
+// 	render() {
+// 		return (
+// 			<button className="square" onClick={this.props.onClick}>
+// 				{this.props.value}
+// 			</button>
+// 		);
+// 	}
+// }
+
+class Board extends React.Component {
+	renderSquare(i) {
+		return (
+			<Square
+				value={this.props.squares[i]}
+				onClick={() => this.props.onClick(i)}
+			/>
+		);
+	}
+
+	render() {
+		return (
+			<div>
+				<div className="board-row">
+					{/* <Square value={0} />
+					<Square value={1} />
+					<Square value={2} /> */}
+					{this.renderSquare(0)}
+					{this.renderSquare(1)}
+					{this.renderSquare(2)}
+				</div>
+				<div className="board-row">
+					{/* <Square value={3} />
+					<Square value={4} />
+					<Square value={5} /> */}
+					{this.renderSquare(3)}
+					{this.renderSquare(4)}
+					{this.renderSquare(5)}
+				</div>
+				<div className="board-row">
+					{/* <Square value={6} />
+					<Square value={7} />
+					<Square value={8} /> */}
+					{this.renderSquare(6)}
+					{this.renderSquare(7)}
+					{this.renderSquare(8)}
+				</div>
+			</div>
+		);
+	}
+}
+
+class Game extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			history: [
+				{
+					squares: Array(9).fill(null),
+				},
+			],
+			stepNumber: 0,
+			xIsNext: true,
+		};
+	}
+
+	handleClick(i) {
+		const history = this.state.history.slice(0, this.state.stepNumber + 1);
+		const current = history[this.state.stepNumber];
+		const squares = [...current.squares];
+		if (calculateWinner(squares) || squares[i]) return;
+		squares[i] = this.state.xIsNext ? "X" : "O";
+		// history.push({ squares: squares }); //this is wrong, because it modifies the array, that is later used in setState method and because of that React moves it to the top of handleClick method???
+
+		this.setState({
+			history: history.concat({ squares: squares }),
+			stepNumber: history.length,
+			xIsNext: !this.state.xIsNext,
+		});
+	}
+
+	jumpTo(move) {
+		this.setState({
+			stepNumber: move,
+			xIsNext: move % 2 === 0,
+		});
+	}
+
+	render() {
+		const history = this.state.history;
+		const current = history[this.state.stepNumber];
+		const moves = history.map((item, move) => {
+			const desc = move ? `Go to move #${move}` : "Go to game start";
+			return (
+				<li key={move}>
+					<button onClick={() => this.jumpTo(move)}>{desc}</button>
+				</li>
+			);
+		});
+
+		const winner = calculateWinner(current.squares);
+		let status;
+		if (winner) status = `Winner: ${winner}`;
+		else status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
+
+		return (
+			<div className="game">
+				<div className="game-board">
+					<Board
+						squares={current.squares}
+						onClick={(i) => this.handleClick(i)}
+					/>
+				</div>
+				<div className="game-info">
+					<div>{status}</div>
+					<ol>{moves}</ol>
+				</div>
+			</div>
+		);
+	}
+}
+
+// ========================================
+
+// ReactDOM.render(<Game />, document.getElementById("root"));
+
+function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+
+	let result = null;
+
+	lines.forEach((line) => {
+		const [a, b, c] = line;
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+			result = squares[a];
+	});
+
+	return result;
+}
+
+const root = ReactDOMClient.createRoot(document.getElementById("root"));
+root.render(<Game />);
